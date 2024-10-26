23803b3fdcb91cd352ece2a55030ade7f914c2dc
iblalahmed12 <iblalahmed12@gmail.com>
2024-10-26 16:39:57 +0100

    updated mot api client to public

--- MOTLookup.Infrastructure/IClients/IMOTApiClient.cs
+++ MOTLookup.Infrastructure/IClients/IMOTApiClient.cs
@@ -3,7 +3,7 @@ using MOTLookup.Models.Shared;
 
 namespace MOTLookup.Infrastructure.IClients
 {
-    internal interface IMOTApiClient
+    public interface IMOTApiClient
     {
         Task<Result<List<MOTAPIResponse>>> GetVehicleDataAsync(string registration);
     }
