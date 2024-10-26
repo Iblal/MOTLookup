24cfd36b230eab7f7132fa88880b60153d924a28
iblalahmed12 <iblalahmed12@gmail.com>
2024-10-26 16:04:41 +0100

    Added result model

---
+++ MOTLookup.Models/Shared/Result.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace MOTLookup.Models.Shared
+{
+    public sealed class Result<T>
+    {
+
+        public bool IsSuccess { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+
+}
