af83b12458ba8afe12edabb25ff17a562497b9c5
iblalahmed12 <iblalahmed12@gmail.com>
2024-10-26 16:04:18 +0100

    updated request model

--- MOTLookup.Models/Requests/VehicleRequest.cs
+++ MOTLookup.Models/Requests/VehicleRequest.cs
@@ -1,12 +1,4 @@
 ﻿namespace MOTLookup.Models.Requests
 {
-    public sealed class VehicleRequest
-    {
-        public VehicleRequest(string registrationNumber)
-        {
-            RegistrationNumber = registrationNumber;
-        }
-
-        public string RegistrationNumber { get; private set; }
-    }
+    public sealed record VehicleRequest(string registration);
 }
