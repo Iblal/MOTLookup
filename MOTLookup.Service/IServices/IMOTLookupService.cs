cf03efc51ec34d9164d944f1e8ee189a3a06d66f
iblalahmed12 <iblalahmed12@gmail.com>
2024-10-26 16:40:47 +0100

    added MOT lookup service

---
+++ MOTLookup.Service/IServices/IMOTLookupService.cs
@@ -0,0 +1,10 @@
+﻿using MOTLookup.Models.Responses;
+using MOTLookup.Models.Shared;
+
+namespace MOTLookup.Service.IServices
+{
+    interface IMOTLookupService
+    {
+        Task<Result<VehicleResponse>> GetVehicleData(string registration);
+    }
+}
---
+++ MOTLookup.Service/Services/MOTLookupService.cs
@@ -0,0 +1,67 @@
+﻿using MOTLookup.Infrastructure.IClients;
+using MOTLookup.Models.Responses;
+using MOTLookup.Models.Shared;
+using MOTLookup.Service.IServices;
+using System.Net;
+
+namespace MOTLookup.Service.Services
+{
+    public class MOTService : IMOTLookupService
+    {
+        private readonly IMOTApiClient _motApiClient;
+
+        public MOTService(IMOTApiClient motApiClient)
+        {
+            _motApiClient = motApiClient;
+        }
+
+        public async Task<Result<VehicleResponse>> GetVehicleData(string registration)
+        {
+            var result = new Result<VehicleResponse>();
+
+            try
+            {
+                var apiResult = await _motApiClient.GetVehicleDataAsync(registration);
+
+                result.StatusCode = apiResult.StatusCode;
+                result.Message = apiResult.Message;
+
+                if (apiResult.IsSuccess && apiResult.Data is not null)
+                {
+                    var apiResponse = apiResult.Data.FirstOrDefault();
+
+                    if (apiResponse is not null)
+                    {
+                        var lastMotTest = apiResponse.MotTests?.OrderByDescending(m => m.ExpiryDate).FirstOrDefault();
+
+                        result.IsSuccess = true;
+                        result.Data = new VehicleResponse(
+                            apiResponse.Make,
+                            apiResponse.Model,
+                            apiResponse.PrimaryColour,
+                            lastMotTest.ExpiryDate,
+                            lastMotTest != null ? lastMotTest.OdometerValue : 0
+                        );
+                    }
+                    else
+                    {
+                        result.IsSuccess = false;
+                        result.Message = "Vehicle data is missing from the response.";
+                    }
+                }
+                else
+                {
+                    result.IsSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccess = false;
+                result.StatusCode = HttpStatusCode.InternalServerError;
+                result.Message = "An unexpected error occurred while processing the data.";
+            }
+
+            return result;
+        }
+    }
+}
