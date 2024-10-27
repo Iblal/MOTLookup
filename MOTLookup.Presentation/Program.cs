0cde29b977d8da3d1a560c57190e865f64616aa2
iblalahmed12 <iblalahmed12@gmail.com>
2024-10-27 09:34:12 +0000

    added presentation project

---
+++ MOTLookup.Presentation/Program.cs
@@ -0,0 +1,50 @@
+using MOTLookup.Infrastructure.IClients;
+using MOTLookup.Models.Settings;
+using MOTLookup.Presentation.Data;
+using MOTLookup.Service.IServices;
+using MOTLookup.Service.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddSingleton<IMOTLookupService, MOTLookupService>();
+builder.Services.AddSingleton<IMOTApiClient, MOTApiClient>();
+builder.Services.Configure<MotApiSettings>(builder.Configuration.GetSection("MotApiSettings"));
+
+var motApiSettings = builder.Configuration.GetSection("MotApiSettings").Get<MotApiSettings>();
+
+builder.Services.AddHttpClient<IMOTApiClient, MOTApiClient>(client =>
+{
+    client.BaseAddress = new Uri(motApiSettings.BaseUrl);
+    client.DefaultRequestHeaders.Add("x-api-key", motApiSettings.ApiKey);
+});
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
